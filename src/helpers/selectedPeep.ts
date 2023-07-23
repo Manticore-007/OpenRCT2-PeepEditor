8019bfc48fc6cf0f5d830b4973ec89240635159d
Manticore-007 <71702374+Manticore-007@users.noreply.github.com>
2023-07-23 13:20:12 +0200

    missed semicolon

--- src/helpers/selectedPeep.ts
+++ src/helpers/selectedPeep.ts
@@ -8,7 +8,7 @@ export let selectedStaffCostume: number = -1;
 export function setSelectedPeep(entity: Guest|Staff): void
 {
     selectedPeep = entity;
-    selectedStaff = <Staff>selectedPeep
+    selectedStaff = <Staff>selectedPeep;
     selectedStaffCostume = selectedStaff.costume;
     selectedStaffType = staffType.indexOf(selectedStaff.staffType);
 }
