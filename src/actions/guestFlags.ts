f896d9ef844f42d0c48413bf6c498fa03b55ff2a
Manticore-007 <71702374+Manticore-007@users.noreply.github.com>
2024-06-02 13:23:12 +0200

    fix for crash when peep got removed from map.

---
+++ src/actions/guestFlags.ts
@@ -0,0 +1,19 @@
+
+export interface GuestFlagsArgs {
+    id: number | null;
+    checked: boolean
+    flag: PeepFlags;
+}
+
+export function guestFlagsExecute(args: GuestFlagsArgs): GameActionResult
+{
+    if (args.id === null){return{}};
+    const entity = map.getEntity(args.id);
+    const guest = <Guest>entity;
+    guest.setFlag(args.flag , args.checked);
+    return{}
+}
+
+export function guestFlagsExecuteArgs(id: number | null, checked: boolean, flag: PeepFlags): GuestFlagsArgs{
+    return {"id": id, "checked": checked, "flag": flag}
+}
